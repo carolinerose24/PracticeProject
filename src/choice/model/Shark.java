fcfe99e8ff1a1578904846e2edc7ee5db80e082c
Caroline Jarman <carolinerosejarman@gmail.com>
2018-11-14 09:53:03 -0700

    Initial commit

---
+++ src/choice/model/Shark.java
@@ -0,0 +1,17 @@
+package choice.model;
+
+public class Shark
+{
+			
+	private String nameOfShark;
+	private int numberOfHeads;
+	private boolean hasModifications;
+	
+	public Shark(String nameOfShark, int numberOfHeads, boolean hasModifications)
+	{
+		this.nameOfShark = nameOfShark;
+		this.numberOfHeads = numberOfHeads;
+		this.hasModifications = hasModifications;
+	}
+	
+}
